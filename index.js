93c2729fa4a6df41b6676a5ea205ee9d61dbff78
Soner Zorlu <sonerzorlu@gmail.com>
2023-10-13 16:10:11 +0300

    UP

--- index.js
+++ index.js
@@ -7,79 +7,48 @@
     $ npm i cookie-session
     $ npm i jsonwebtoken
 */
-
 const express = require('express')
 const app = express()
-
 /* ------------------------------------------------------- */
-// Required Modules:
-
-// envVariables to process.env:
 require('dotenv').config()
 const PORT = process.env?.PORT || 8000
 
-// asyncErrors to errorHandler:
+//asyncError to errorHandler
 require('express-async-errors')
-
 /* ------------------------------------------------------- */
-// Configrations:
+//!Configrations
 
-// Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
+//Connect to the DB
+const {dbConnection } = require('./src/configs/dbConnection')
 dbConnection()
-
 /* ------------------------------------------------------- */
-// Middlewares:
+//!MIDDLEWARES
+
 
 // Accept JSON:
 app.use(express.json())
-
-// SessionsCookies:
-app.use(require('cookie-session')({ secret: process.env.SECRET_KEY }))
-
-// res.getModelList():
+//Sessionscookies:
+app.use(require('cookie-session')({secret: process.env.SECRET_KEY}))
+//res.getModelList():
 app.use(require('./src/middlewares/findSearchSortPage'))
 
-// Login/Logout Control Middleware
-app.use(async (req, res, next) => {
-
-    const Personnel = require('./src/models/personnel.model')
-
-    req.isLogin = false
-
-    if (req.session?.id) {
-
-        const user = await Personnel.findOne({ _id: req.session.id })
-
-        // if (user && user.password == req.session.password) {
-        //     req.isLogin = true
-        // }
-        req.isLogin = user && user.password == req.session.password
-    }
-    console.log('isLogin: ', req.isLogin)
-
-    next()
-})
-
 /* ------------------------------------------------------- */
-// Routes:
+//!ROUTES
 
-// HomePath:
-app.all('/', (req, res) => {
-    res.send({
-        error: false,
-        message: 'Welcome to PERSONNEL API',
-        session: req.session,
-        isLogin: req.isLogin
+//HomePath'/':
+    app.all('/',(req,res)=>{
+        res.send({
+            error:false,
+            message: 'Welcome to PERSONAL API'
+        })
     })
-})
 
-// /departments
-app.use('/departments', require('./src/routes/department.router'))
-// /personnels
-app.use('/personnels', require('./src/routes/personnel.router'))
+ 
 
 /* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+
 
 // errorHandler:
 app.use(require('./src/middlewares/errorHandler'))
--- src/models/department.model.js
+++ src/models/department.model.js
@@ -4,17 +4,3 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
-
-const DepartmentSchema = new mongoose.Schema({
-
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true
-    }
-
-}, { collection: "departments", timestamps: true })
-
-/* ------------------------------------------------------- */
-module.exports = mongoose.model('Department', DepartmentSchema)
