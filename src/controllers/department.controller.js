2bd427e4feb44f4023f454a3892c7246beb49625
Soner Zorlu <sonerzorlu@gmail.com>
2023-10-13 17:44:39 +0300

    departmentController

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -1,4 +1,52 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = require("./src/models/department.models");
+
+module.exports = {
+  list: async (req, res) => {
+    // const data = await Department.find(search).sort(sort).skip(skip).limit(limit)
+    const data = await res.getModdelList(Department);
+    res.status(200).send({
+      error: false,
+      // data:data
+      data,
+    });
+  },
+
+  create: async (req, res) => {
+    const data = await Department.create(req, res);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+
+  read: async (req, res) => {
+    const data = await Derpartment.findOne({_id:req.params.id})
+    res.status(200).send({
+        error: false,
+        data,
+      });
+  },
+
+  update: async (req, res) => {
+    const data = await Department.updateOne({_id: req.params.id},req.body)
+    res.status(202).send({
+        error: false,
+        data,
+        new: await Department.findOne({_id:req.params.id})
+      });
+  },
+
+  delete: async (req, res) => {
+    const data = await Department.deleteOne({_id:req.params.id})
+
+    res.status(data.deletedCount>=1 ? 204:404).send({
+        error: false,
+        data,
+      });
+  },
+  }
--- src/models/department.model.js
+++ src/models/department.model.js
@@ -1,6 +1,17 @@
 "use strict"
 /* -------------------------------------------------------
     EXPRESS - Personnel API
-------------------------------------------------------- */
+------------------------------------------------------ */
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+/* --------------------------- --------------------------- */
+const DepartmentSchema = new mongoose.Schema({
+
+    namw:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true
+    }
+},{collection:"departments",timestamps:trure})
+
+module.exports=mongoose.model("Department",DepartmentSchema)
